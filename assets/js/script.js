6fc40738185a8caecb090c8d673e8795c13d34d9
Francisco Oliva <francisco.j.oliva@gmail.com>
2021-10-02 14:00:58 -0300

    Added Additional JS functionality under Destacados section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
+// 1st JS functionality - Tooltip on Quienes Somos Columns Icons.
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
+// 2nd JS functionality - Toggle Text On and Off by clicking the Title under Destacados section.
+const h5_card_title_1_fx = document.getElementById("h5_card_title_1_fx");
+const p_card_text_1_fx = document.getElementById("p_card_text_1_fx");
+
+h5_card_title_1_fx.addEventListener("click", function(){
+    p_card_text_1_fx.classList.toggle("d-none");
+});
+
+const h5_card_title_2_fx = document.getElementById("h5_card_title_2_fx");
+const p_card_text_2_fx = document.getElementById("p_card_text_2_fx");
+
+h5_card_title_2_fx.addEventListener("click", function(){
+    p_card_text_2_fx.classList.toggle("d-none");
+});
+
+const h5_card_title_3_fx = document.getElementById("h5_card_title_3_fx");
+const p_card_text_3_fx = document.getElementById("p_card_text_3_fx");
+
+h5_card_title_3_fx.addEventListener("click", function(){
+    p_card_text_3_fx.classList.toggle("d-none");
+});
+
+const h5_card_title_4_fx = document.getElementById("h5_card_title_4_fx");
+const p_card_text_4_fx = document.getElementById("p_card_text_4_fx");
+
+h5_card_title_4_fx.addEventListener("click", function(){
+    p_card_text_4_fx.classList.toggle("d-none");
+});
